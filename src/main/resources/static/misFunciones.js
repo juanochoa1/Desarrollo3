91eca41e1528ad79ac533b1373d629f3f6045574
Cristian <cristian.ochoa.mt@usa.edu.co>
2022-10-13 14:05:32 -0500

    TercerCommit

--- src/main/resources/static/misFunciones.js
+++ src/main/resources/static/misFunciones.js
@@ -1,24 +1,37 @@
 function traerDatosCategoria(){
     $.ajax( {
-        url: "http://localhost:8080/api/Reservation/all",
+        url: "http://150.136.155.195:8080/api/Category/all",
         type: "GET",
         dataType: "json",
         success: function(respuesta){
             pintarDatos(respuesta);
         },
         error: function(respuesta, xhr){
-            alert("Error De Petición");
+            alert("Error De Peticion!");
         }
+
     });
 }
 
 function pintarDatos(datos){
-    let html=""
+    let html="";
     html += "<tr>";
     Object.keys(datos[0]).forEach(elemento =>{
         html+="<th>"+elemento+"</th>";
     });
     html += "</tr>";
+
+    for(let i=0; i<datos.length; i++){
+        html += "<tr>";
+        Object.values(datos[i]).forEach(elemento =>{
+            if(typeof(elemento)=='object') {
+                html += "<td>" + elemento[0].name + "</td>";
+            }else {
+                html += "<td>" + elemento + "</td>";
+            }
+        });
+        html += "</tr>";
+    }
 
     $("#tabla").empty();
     $("#tabla").append(html);
